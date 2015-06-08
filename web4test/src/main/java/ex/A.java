47367ecca206ced2c199aa26d73409dedc6710c0
kohyusik <newtype26@naver.com>
2015-06-08 14:36:13 +0900

    ,.

--- web4test/src/main/java/ex/A.java
+++ web4test/src/main/java/ex/A.java
@@ -3,11 +3,13 @@ package ex;
 public class A {
 
   public static void main(String[] args) {
+    int a = 60;
+    System.out.println("고유식.!.");
+    System.out.println("홍길동2222222");
+    int b = 60;
+    System.out.println("우인규!@@@@!!!");
     
-    System.out.println("홍길동");
-    System.out.println("우인규!!!!!!");
     
-    int a = 20;
     if (a < 20) {
       System.out.println("okok");
     }
