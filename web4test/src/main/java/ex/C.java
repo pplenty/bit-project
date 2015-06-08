6eec467d2dc7a8a9f59ac075fdeea41184be06f7
kohyusik <newtype26@naver.com>
2015-06-08 15:33:58 +0900

    init

--- web4test/src/main/java/ex/C.java
+++
@@ -1,9 +0,0 @@
-package ex;
-
-public class C {
-
-  public static void main(String[] args) {
-    System.out.println("이혜인");
-  }
-
-}
--- web4test/src/main/java/ex/D.java
+++
@@ -1,9 +0,0 @@
-package ex;
-
-public class D {
-
-  public static void main(String[] args) {
-    System.out.println("test..ok");
-  }
-
-}
