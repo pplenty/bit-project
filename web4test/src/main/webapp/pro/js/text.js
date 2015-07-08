f2dd700a6cbbb23e15d60769afa3f3a3c30c9b82
hyeinlee622 <hyeinlee622@gmail.com>
2015-07-08 12:02:45 +0900

    text.js 수정

--- web4test/src/main/webapp/pro/js/text.js
+++ web4test/src/main/webapp/pro/js/text.js
@@ -70,7 +70,7 @@ $(function(){
 // 사이드바 입력값 isFocus에 맞추어 초기 설정하기	    
 	    // 폰트
 	    var initFontSize = pxRemove(contentBox.css('font-size'));
-	    $(".font.size-box").val(initFontSize);
+	    $(".text.font.size-box").val(initFontSize);
 	    //폰트 색깔
 	    var initFontColor = rgb2hex($('.sl-block.isFocus p').css('color'));
 	    console.log(initFontColor)
@@ -80,28 +80,28 @@ $(function(){
 	    $('.back-colorinput').val(initBackColor);
 	    //opacity
 	    var initOpacity = $('.sl-block.isFocus p').css('opacity');
-	    $('opacity.size-scroll').val(initOpacity);
-	    $('.opacity.size-box').val(initOpacity);
+	    $('.text.opacity.size-scroll').val(initOpacity);
+	    $('.text.opacity.size-box').val(initOpacity);
  	    // 패딩값
 	    var initPaddingSize = pxRemove(contentBox.css('padding'));
-	    $(".padding.size-box").val(initPaddingSize);
+	    $(".text.padding.size-box").val(initPaddingSize);
 	    // 선 유무 체크 박스
 	    var initBorderYn = rgb2hex($(".sl-block.isFocus").css('border-color')); 
 	    console.log(initBorderYn);
 	    if(initBorderYn == '#ffffff'){ //투명값일 경우
-	    	$('.border.checkbox').prop('checked', false) // 체크박스에 표시 안되게 해야함
+	    	$('.text.border.checkbox').prop('checked', false) // 체크박스에 표시 안되게 해야함
 	    } else {
-	    	$('.border.checkbox').prop('checked', 'true') // 체크박스에 표시하기 
+	    	$('.text.border.checkbox').prop('checked', 'true') // 체크박스에 표시하기 
 	    }
 	    // 선 두께 lineWidth
 	    var initBorderWidth = pxRemove($(this).css('border-width'));
-	    $(".lineWidth.size-box").val(initBorderWidth);
+	    $(".text.lineWidth.size-box").val(initBorderWidth);
 	    //선 색깔
 	    var initBorderColor = rgb2hex($('.sl-block.isFocus').css('border-color'));
 	    $('.text-border-colorinput').val(initBorderColor);
 	    // 곡선값
 	    var initRadius = pxRemove($(this).css('border-radius'));
-	    $(".lineRadius.size-box").val(initRadius);
+	    $(".text.lineRadius.size-box").val(initRadius);
 	    
 // 사이드바 보이게 하기    
 	    $(".toolbar-list").css('visibility', 'hidden');
@@ -148,56 +148,56 @@ $(function(){
 	    	var middle;
 	    	
 ////////-font-size 스크롤 조정
-	$(".font.size-box").on('mousedown', function(event){
+	$(".text.font.size-box").on('mousedown', function(event){
 		objContent = $(".isFocus").children('.sl-block-content')
 		pxFontSize = objContent.css('font-size');
 		numFontSize = pxRemove(pxFontSize);
-		$(".font.size-box").val(numFontSize);
+		$(".text.font.size-box").val(numFontSize);
 	    preX = event.offsetX;
-	    i = parseInt($(".font.size-box").val());
+	    i = parseInt($(".text.font.size-box").val());
 	    fontFlag = true;
 	    	
-	    	$(".font.size-box").on('mousemove', function(ev){
+	    	$(".text.font.size-box").on('mousemove', function(ev){
 	    		if(fontFlag == true){ // 마우스 무브 
 	    		console.log("digh")
 	    		moveX = ev.offsetX;
 	    		if(preX < moveX){
 	    				 if(i < 100){
 	    					 console.log("right")
-	    					 i = parseInt($(".font.size-box").val());
+	    					 i = parseInt($(".text.font.size-box").val());
 	    					 console.log(i)
-	    					 middle = $(".font.size-box").val(i+1);
+	    					 middle = $(".text.font.size-box").val(i+1);
 	    					 objContent.css('font-size', middle + 'px');
 	    				 }// 증가 범위 설정
 	    				 else if (i > 100){
-	    					 $(".font.size-box").val(100);
+	    					 $(".text.font.size-box").val(100);
 	    				 } // 최고점 100 마지막 괄호
 	    		}// 오른쪽 으로 움직일 때 닫는 괄호
 	    		else if(moveX < preX){ // 왼쪽으로 움직일 때
 	    			if (i > 1){ // 일반적 감소
-	    				i = parseInt($(".font.size-box").val());
-	    				middle = $(".font.size-box").val(i -1);	    					 
+	    				i = parseInt($(".text.font.size-box").val());
+	    				middle = $(".text.font.size-box").val(i -1);	    					 
 	    				objContent.css('font-size', middle + 'px'); 				
 	    				}// 감소할 때 값 마지막 괄호
 	    			else if (i <= 1){
-	    				i = parseInt($(".font.size-box").val());
-	    				$(".font.size-box").val(1);
+	    				i = parseInt($(".text.font.size-box").val());
+	    				$(".text.font.size-box").val(1);
 	    				}// 감소 최저값
 	    		} // 왼쪽으로 움직일 때 마지막 괄호
 	     }// 마우스 무브 열림 내용 닫는 괄호
 	});// 마우스 무브 마지막 괄호
 });
-	$(".font.size-box").on('mouseup', function(){
+	$(".text.font.size-box").on('mouseup', function(){
 		fontFlag = false;
 		preX = '0';
-		$(".font.size-box").blur();
-		fontFinalInput = $(".font.size-box").val();
+		$(".text.font.size-box").blur();
+		fontFinalInput = $(".text.font.size-box").val();
 		objContent.css('font-size', fontFinalInput + 'px');
 	});// 마우스 업일 때 동작 해제
 	
-	$(".font.size-box").on('dblclick', function(){ // 직접 입력받기
-		$(".font.size-box").focus();
-		fontFinalInput = $(".font.size-box").val();
+	$(".text.font.size-box").on('dblclick', function(){ // 직접 입력받기
+		$(".text.font.size-box").focus();
+		fontFinalInput = $(".text.font.size-box").val();
 		objContent.css('font-size', fontFinalInput +'px');
 	}); // 직접 입력 받기
 	
@@ -217,37 +217,37 @@ $(function(){
 			//-투명도 값 스크롤 조정
 			var opSizeInput;
 			var opFlag = false;
-			$(".opacity.size-scroll").on('mousedown', function(event){
+			$(".text.opacity.size-scroll").on('mousedown', function(event){
 				opFlag = true;
 				if(opFlag == true){
-				$(".opacity.size-scroll").on("mousemove", function(ev){	
+				$(".text.opacity.size-scroll").on("mousemove", function(ev){	
 					opSizeInput = $(ev.target).val();
-					$(".opacity.size-box").val(opSizeInput);
+					$(".text.opacity.size-box").val(opSizeInput);
 				}); 
 				}
 			})
 			
 			//-투명도값 관리 
-			$(".opacity.size-scroll").on("mouseup", function(){
-				$(".opacity.size-box").val(opSizeInput );
+			$(".text.opacity.size-scroll").on("mouseup", function(){
+				$(".text.opacity.size-box").val(opSizeInput );
 				opFlag = false;
 		
 			});
 			//- 투명도값 직접 입력받기
-			 $(".opacity.size-scroll").on("dblclick", function(ev){
+			 $(".text.opacity.size-scroll").on("dblclick", function(ev){
 				$(this).css("z-index", "0");
 				$(this).addClass("back");
-				$(".opacity.size-box").focus();
-				opSizeInput = $(".opacity.size-box").val();			
+				$(".text.opacity.size-box").focus();
+				opSizeInput = $(".text.opacity.size-box").val();			
 			}); 
 			 // - 투명도 입력 완료
-			 $(document).not($(".opacity.size-box")).click(function(){
-			    	$(".opacity.size-scroll").css("z-index", "16");
-			    	$(".opacity.size-scroll").removeClass("back");
+			 $(document).not($(".text.opacity.size-box")).click(function(){
+			    	$(".text.opacity.size-scroll").css("z-index", "16");
+			    	$(".text.opacity.size-scroll").removeClass("back");
 			    });
 
 			//투명도값 적용
-				$('.opacity.size-scroll').on('change', function(){	
+				$('.text.opacity.size-scroll').on('change', function(){	
 					$('.sl-block.isFocus .sl-block-content p').css({'opacity': $(this).val()});
 				})
 
@@ -265,54 +265,54 @@ $(function(){
 		    	//var middle;
 		    	
 	////////-padding-size 스크롤 조정
-		$(".padding.size-box").on('mousedown', function(event){
+		$(".text.padding.size-box").on('mousedown', function(event){
 			objContent = $(".isFocus").children('.sl-block-content')
 			pxPaddingSize = objContent.css('padding');
 			numPaddingSize = pxRemove(pxPaddingSize);
-			$(".padding.size-box").val(numPaddingSize);
+			$(".text.padding.size-box").val(numPaddingSize);
 		    preX = event.offsetX;
-		    i = parseInt($(".padding.size-box").val());
+		    i = parseInt($(".text.padding.size-box").val());
 		    paddingFlag = true;
 		    	
-		    	$(".padding.size-box").on('mousemove', function(ev){
+		    	$(".text.padding.size-box").on('mousemove', function(ev){
 		    		if(paddingFlag == true){ // 마우스 무브 
 		    		moveX = ev.offsetX;
 		    		if(preX < moveX){
 		    				 if(i < 100){
-		    					 i = parseInt($(".padding.size-box").val());
-		    					 middle = $(".padding.size-box").val(i+1);
+		    					 i = parseInt($(".text.padding.size-box").val());
+		    					 middle = $(".text.padding.size-box").val(i+1);
 		    					 objContent.css('padding', middle + 'px');
 		    				 }// 증가 범위 설정
 		    				 else if (i > 100){
-		    					 $(".padding.size-box").val(100);
+		    					 $(".text.padding.size-box").val(100);
 		    				 } // 최고점 100 마지막 괄호
 		    		}// 오른쪽 으로 움직일 때 닫는 괄호
 		    		else if(moveX < preX){ // 왼쪽으로 움직일 때
 		    			if (i > 1){ // 일반적 감소
-		    				i = parseInt($(".padding.size-box").val());
-		    				middle = $(".padding.size-box").val(i -1);	    					 
+		    				i = parseInt($(".text.padding.size-box").val());
+		    				middle = $(".text.padding.size-box").val(i -1);	    					 
 		    				objContent.css('padding', middle + 'px'); 				
 		    				}// 감소할 때 값 마지막 괄호
 		    			else if (i <= 0){
-		    				i = parseInt($(".padding.size-box").val());
-		    				$(".padding.size-box").val(0);
+		    				i = parseInt($(".text.padding.size-box").val());
+		    				$(".text.padding.size-box").val(0);
 		    				}// 감소 최저값
 		    		} // 왼쪽으로 움직일 때 마지막 괄호
 		     }// 마우스 무브 열림 내용 닫는 괄호
 		});// 마우스 무브 마지막 괄호
 	});
-		$(".padding.size-box").on('mouseup', function(){
+		$(".text.padding.size-box").on('mouseup', function(){
 			paddingFlag = false;
 			preX = '0';
-			$(".padding.size-box").blur();
-			paddingFinalInput = $(".padding.size-box").val();
+			$(".text.padding.size-box").blur();
+			paddingFinalInput = $(".text.padding.size-box").val();
 			objContent.css('padding', paddingFinalInput + 'px');
 		});// 마우스 업일 때 동작 해제
 		
-		$(".padding.size-box").on('dblclick', function(){ // 직접 입력받기
-			$(".padding.size-box").focus();
-			paddingFinalInput = $(".padding.size-box").val();
-			$(".padding.size-box").blur(function(){
+		$(".text.padding.size-box").on('dblclick', function(){ // 직접 입력받기
+			$(".text.padding.size-box").focus();
+			paddingFinalInput = $(".text.padding.size-box").val();
+			$(".text.padding.size-box").blur(function(){
 				console.log("digh")
 				objContent.css('padding', paddingFinalInput +'px');
 			});
@@ -327,8 +327,8 @@ $(function(){
 			var line = lineStyle + ' ' + lineWidth + 'px ' +  lineRadius +'px '+ lineColor+';'
 			
 //	// 디폴트 선 설정 및 적용
-			$('.checkbox').on('click', function(){
-				if($('.border.checkbox').prop('checked')){
+			$('.text.border.checkbox').on('click', function(){
+				if($('.text.border.checkbox').prop('checked')){
 					$(".sl-block.isFocus").css('border-width','1px');
 					$(".sl-block.isFocus").css('border-style','solid');
 					$(".sl-block.isFocus").css('border-color','black');
@@ -355,54 +355,54 @@ $(function(){
 		    	//var middle;
 		    	
 	////////-lineWidth-size 스크롤 조정
-		$(".lineWidth.size-box").on('mousedown', function(event){
+		$(".text.lineWidth.size-box").on('mousedown', function(event){
 			objContent = $(".isFocus").children('.sl-block-content')
 			pxlineWidthSize = objContent.css('border-width');
 			numlineWidthSize = pxRemove(pxlineWidthSize);
-			$(".lineWidth.size-box").val(numlineWidthSize);
+			$(".text.lineWidth.size-box").val(numlineWidthSize);
 		    preX = event.offsetX;
-		    i = parseInt($(".lineWidth.size-box").val());
+		    i = parseInt($(".text.lineWidth.size-box").val());
 		    lineWidthFlag = true;
 		    	
-		    	$(".lineWidth.size-box").on('mousemove', function(ev){
+		    	$(".text.lineWidth.size-box").on('mousemove', function(ev){
 		    		if(lineWidthFlag == true){ // 마우스 무브 
 		    		moveX = ev.offsetX;
 		    		if(preX < moveX){
 		    				 if(i < 100){
-		    					 i = parseInt($(".lineWidth.size-box").val());
-		    					 middle = $(".lineWidth.size-box").val(i+1);
+		    					 i = parseInt($(".text.lineWidth.size-box").val());
+		    					 middle = $(".text.lineWidth.size-box").val(i+1);
 		    					 objContent.css('border-width', middle + 'px');
 		    				 }// 증가 범위 설정
 		    				 else if (i > 100){
-		    					 $(".lineWidth.size-box").val(100);
+		    					 $(".text.lineWidth.size-box").val(100);
 		    				 } // 최고점 100 마지막 괄호
 		    		}// 오른쪽 으로 움직일 때 닫는 괄호
 		    		else if(moveX < preX){ // 왼쪽으로 움직일 때
 		    			if (i > 1){ // 일반적 감소
-		    				i = parseInt($(".lineWidth.size-box").val());
-		    				middle = $(".lineWidth.size-box").val(i -1);	    					 
+		    				i = parseInt($(".text.lineWidth.size-box").val());
+		    				middle = $(".text.lineWidth.size-box").val(i -1);	    					 
 		    				objContent.css('border-width', middle + 'px'); 				
 		    				}// 감소할 때 값 마지막 괄호
 		    			else if (i <= 0){
-		    				i = parseInt($(".lineWidth.size-box").val());
-		    				$(".lineWidth.size-box").val(0);
+		    				i = parseInt($(".text.lineWidth.size-box").val());
+		    				$(".text.lineWidth.size-box").val(0);
 		    				}// 감소 최저값
 		    		} // 왼쪽으로 움직일 때 마지막 괄호
 		     }// 마우스 무브 열림 내용 닫는 괄호
 		});// 마우스 무브 마지막 괄호
 	});
-		$(".lineWidth.size-box").on('mouseup', function(){
+		$(".text.lineWidth.size-box").on('mouseup', function(){
 			lineWidthFlag = false;
 			preX = '0';
-			$(".lineWidth.size-box").blur();
-			lineWidth = $(".lineWidth.size-box").val();
+			$(".text.lineWidth.size-box").blur();
+			lineWidth = $(".text.lineWidth.size-box").val();
 			$(".sl-block.isFocus").css('border-width', lineWidth + 'px');
 		});// 마우스 업일 때 동작 해제
 		
-		$(".lineWidth.size-box").on('dblclick', function(){ // 직접 입력받기
-			$(".lineWidth.size-box").focus();
-			lineWidth = $(".lineWidth.size-box").val();
-			$(".lineWidth.size-box").blur(function(){
+		$(".text.lineWidth.size-box").on('dblclick', function(){ // 직접 입력받기
+			$(".text.lineWidth.size-box").focus();
+			lineWidth = $(".text.lineWidth.size-box").val();
+			$(".text.lineWidth.size-box").blur(function(){
 				console.log("digh")
 				$(".sl-block.isFocus").css('border-width', lineWidth+'px');
 			});
@@ -413,9 +413,9 @@ $(function(){
 				borderColor = $(this).val();
 			    var borderColorCheck = rgb2hex(borderColor); 
 			    if(borderColorCheck == '#ffffff'){ //투명값일 경우
-			    	$('.border.checkbox').prop('checked', false) // 체크박스에 표시 안되게 해야함
+			    	$('.text.border.checkbox').prop('checked', false) // 체크박스에 표시 안되게 해야함
 			    } else {
-			    	$('.border.checkbox').prop('checked', 'true') // 체크박스에 표시하기 
+			    	$('.text.border.checkbox').prop('checked', 'true') // 체크박스에 표시하기 
 			    }
 				$('.sl-block.isFocus').css('border-color', borderColor);			
 			});
@@ -427,54 +427,54 @@ $(function(){
 	    	var numlineRadiusSize;
 	    	
 ////////-lineRadius-size 스크롤 조정
-	$(".lineRadius.size-box").on('mousedown', function(event){
+	$(".text.lineRadius.size-box").on('mousedown', function(event){
 		pxlineRadiusSize = objContent.css('border-radius');
 		numlineRadiusSize = pxRemove(pxlineRadiusSize);
-		$(".lineRadius.size-box").val(numlineRadiusSize);
+		$(".text.lineRadius.size-box").val(numlineRadiusSize);
 	    preX = event.offsetX;
-	    i = parseInt($(".lineRadius.size-box").val());
+	    i = parseInt($(".text.lineRadius.size-box").val());
 	    lineRadiusFlag = true;
 	    	
-	    	$(".lineRadius.size-box").on('mousemove', function(ev){
+	    	$(".text.lineRadius.size-box").on('mousemove', function(ev){
 	    		if(lineRadiusFlag == true){ // 마우스 무브 
 	    		moveX = ev.offsetX;
 	    		if(preX < moveX){
 	    				 if(i < 100){
-	    					 i = parseInt($(".lineRadius.size-box").val());
-	    					 middle = $(".lineRadius.size-box").val(i+1);
+	    					 i = parseInt($(".text.lineRadius.size-box").val());
+	    					 middle = $(".text.lineRadius.size-box").val(i+1);
 	    					 objContent.css('border-radius', middle + 'px');
 	    				 }// 증가 범위 설정
 	    				 else if (i > 100){
-	    					 $(".lineRadius.size-box").val(100);
+	    					 $(".text.lineRadius.size-box").val(100);
 	    				 } // 최고점 100 마지막 괄호
 	    		}// 오른쪽 으로 움직일 때 닫는 괄호
 	    		else if(moveX < preX){ // 왼쪽으로 움직일 때
 	    			if (i > 1){ // 일반적 감소
-	    				i = parseInt($(".lineRadius.size-box").val());
-	    				middle = $(".lineRadius.size-box").val(i -1);	    					 
+	    				i = parseInt($(".text.lineRadius.size-box").val());
+	    				middle = $(".text.lineRadius.size-box").val(i -1);	    					 
 	    				objContent.css('border-radius', middle + 'px'); 				
 	    				}// 감소할 때 값 마지막 괄호
 	    			else if (i <= 0){
-	    				i = parseInt($(".lineRadius.size-box").val());
-	    				$(".lineRadius.size-box").val(0);
+	    				i = parseInt($(".text.lineRadius.size-box").val());
+	    				$(".text.lineRadius.size-box").val(0);
 	    				}// 감소 최저값
 	    		} // 왼쪽으로 움직일 때 마지막 괄호
 	     }// 마우스 무브 열림 내용 닫는 괄호
 	});// 마우스 무브 마지막 괄호
 });// 마우스 다운 끝
 	
-$(".lineRadius.size-box").on('mouseup', function(){
+$(".text.lineRadius.size-box").on('mouseup', function(){
 		lineRadiusFlag = false;
 		preX = '0';
-		$(".lineRadius.size-box").blur();
+		$(".text.lineRadius.size-box").blur();
 		lineRadius = $(".lineRadius.size-box").val();
 		$(".sl-block.isFocus").css('border-radius', lineRadius + 'px');
 });// 마우스 업일 때 동작 해제
 	
-	$(".lineRadius.size-box").on('dblclick', function(){ // 직접 입력받기
-		$(".lineRadius.size-box").focus();
-		lineWidth = $(".lineRadius.size-box").val();
-		$(".lineRadius.size-box").blur(function(){
+	$(".text.lineRadius.size-box").on('dblclick', function(){ // 직접 입력받기
+		$(".text.lineRadius.size-box").focus();
+		lineWidth = $(".text.lineRadius.size-box").val();
+		$(".text.lineRadius.size-box").blur(function(){
 			console.log("digh")
 			$(".sl-block.isFocus").css('border-radius', lineRadius+'px');
 		});
