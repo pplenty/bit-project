d96261a42935c39a8419607472e4f2c535841873
hyeinlee622 <hyeinlee622@gmail.com>
2015-07-15 09:18:22 +0900

    mypage에서 제목수정하기 추

--- web4test/src/main/java/net/bitacademy/java67/web/MypageController.java
+++ web4test/src/main/java/net/bitacademy/java67/web/MypageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import net.bitacademy.java67.dao.MypageDao;
 import net.bitacademy.java67.domain.PresentationVo;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mysql.jdbc.PreparedStatement.ParseInfo;
+
 @Controller
 public class MypageController {
   @Autowired
@@ -22,16 +25,11 @@ public class MypageController {
   @RequestMapping(value="/mypageLoad", method = RequestMethod.POST)
   public Object list(HttpServletRequest request, HttpServletResponse response){
     
-// 접속자 확인작업
-    System.out.println("mypageLoad 진입");
-    System.out.println("사용자 번호 받아오기");
-    // email 주소를 파라미터로 받아서 리스트 요청
+    // ajax 요청 받은 데이터 (사용자 번호, 이메일) 저장하기
     String email = request.getParameter("email");
-    System.out.println(email);
-    // 이메일을 가지고 사용자 번호를 가져와 저장해둔다. 
-    int userNo = mypageDao.selectUser(email);
-    System.out.println("사용자 고유번호" + userNo);
- 
+    System.out.println(email + "이 로그인");
+    int userNo = Integer.parseInt(request.getParameter("userNo"));
+    
 // 사용자 번호가지고 LIST 뽑아 오기
      HashMap<String,Object> sqlParams = new HashMap<String,Object>();//요청할 때 쓸 정보 공간
      sqlParams.put("user_no", userNo);
@@ -51,15 +49,19 @@ public class MypageController {
         // Mywhole 슬라이드 전체 (거꾸로 할껀지, 1번부터 할건지 고민해보기)
         List<PresentationVo> wholeList = mypageDao.selectList(userNo);
         responseData.put("wholeData", wholeList);
+        System.out.println("전체 슬라이드 긁어옴");
         } 
         
       // 공유 슬라이드 중에 즐겨 찾기 해둔 슬라이드 찾기
-      List<PresentationVo> favoriteList = mypageDao.selectFavorite(userNo);     
+      List<PresentationVo> favoriteList = mypageDao.selectFavorite(userNo);  
+      System.out.println(favoriteList+"즐겨찾기 해온 결과!");
+      
         if(favoriteList.isEmpty() == true ){// 즐겨찾기한 내용이 없을 경우
           System.out.println("즐겨찾기한 내용이 없음");
           responseData.put("favoriteEmpty","yes");
         } else { // 즐겨찾기한 내용이 있을 경우
           responseData.put("favoritList", favoriteList);
+          System.out.println("즐겨찾기 찾아옴");
         }
         
       // 전체 슬라이드 중에 공유 설정해둔 슬라이드 찾기
@@ -73,4 +75,26 @@ public class MypageController {
       return responseData;
   
   }
+  
+  
+  @RequestMapping(value="/editTitle", method = RequestMethod.POST)
+  public Object editTitle(HttpServletRequest request, HttpServletResponse response){
+    
+    int preNo = Integer.parseInt(request.getParameter("preNo"));    
+    String newTitle = request.getParameter("newTitle");
+    System.out.println("프레젠테이션 번호" + preNo +"번   새 제목 :" + newTitle);
+    
+    HashMap<String,Object> sqlParams = new HashMap<String,Object>();//요청할 때 쓸 정보 공간
+    sqlParams.put("title", newTitle);
+    sqlParams.put("preNo", preNo);
+    
+    mypageDao.updateTitle(sqlParams);
+
+    HashMap<String, Object> responseData = new HashMap<String, Object>(); // 클라이언트에 보낼 데이터 저장공간
+    responseData.put("status", "success");
+    
+    
+    return responseData;
+  }
+  
 }
