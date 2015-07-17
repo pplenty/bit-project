2823ff8b997a3f6aebd7ce57684fc0c27cacc60d
kohyusik <newtype26@naver.com>
2015-07-17 16:50:13 +0900

    d

---
+++ web4test/src/main/webapp/edit/js/main-ui.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.set').click(function() {
+//		$('.setMemo').css('visibility', 'visible');
+//		$('.setMemo').toggle(true, false);
+		$('.setMemo').toggle('fast');
+	});
+	
+});
